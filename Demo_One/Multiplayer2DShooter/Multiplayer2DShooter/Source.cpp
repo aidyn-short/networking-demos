1375b08ec60f4258696567a791c00221a9bd8553
Aidyn Short <92755815+Arshort5@users.noreply.github.com>
2025-02-25 12:36:06 -0500

    Started learning SDL

--- Demo_One/Multiplayer2DShooter/Multiplayer2DShooter/Source.cpp
+++ Demo_One/Multiplayer2DShooter/Multiplayer2DShooter/Source.cpp
@@ -1,69 +1,100 @@
-/* clear.c ... */
-
-/*
- * This example code creates an SDL window and renderer, and then clears the
- * window to a different color every frame, so you'll effectively get a window
- * that's smoothly fading between colors.
- *
- * This code is public domain. Feel free to use it for any purpose!
- */
-
-#define SDL_MAIN_USE_CALLBACKS 1  /* use the callbacks instead of main() */
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
+#include <iostream>
 
- /* We will use this renderer to draw into this window every frame. */
-static SDL_Window* window = NULL;
-static SDL_Renderer* renderer = NULL;
 
-/* This function runs once at startup. */
-SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[])
-{
-    SDL_SetAppMetadata("Example Renderer Clear", "1.0", "com.example.renderer-clear");
+SDL_Window* gWindow = NULL;
 
-    if (!SDL_Init(SDL_INIT_VIDEO)) {
-        SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
-        return SDL_APP_FAILURE;
-    }
+SDL_Surface* gScreenSurface = NULL;
 
-    if (!SDL_CreateWindowAndRenderer("examples/renderer/clear", 640, 480, 0, &window, &renderer)) {
-        SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
-        return SDL_APP_FAILURE;
-    }
+SDL_Surface* gImage = NULL;
 
-    return SDL_APP_CONTINUE;  /* carry on with the program! */
+const int SCREEN_HEIGHT = 480;
+const int SCREEN_WIDTH = 640;
+
+bool init() {
+
+	bool success = true;
+
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		std::cout << "Couldn't initalize";
+		success = false;
+
+	}
+	else
+	{
+		gWindow = SDL_CreateWindow("SDL Tutorial", SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+		if (gWindow == NULL)
+		{
+			std::cout << "Window couldn't be created";
+			success = false;
+		}
+		else
+		{
+			gScreenSurface = SDL_GetWindowSurface(gWindow);
+		}
+
+	}
+
+
+	return success;
 }
 
-/* This function runs when a new event (mouse input, keypresses, etc) occurs. */
-SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event)
-{
-    if (event->type == SDL_EVENT_QUIT) {
-        return SDL_APP_SUCCESS;  /* end the program, reporting success to the OS. */
-    }
-    return SDL_APP_CONTINUE;  /* carry on with the program! */
+bool loadMedia() {
+
+	bool success = true;
+
+	gImage = SDL_LoadBMP("hello_world.bmp");
+	if (gImage == NULL)
+	{
+		std::cout << "Unable to load image";
+		success = false;
+	}
+
+	return success;
+
+
 }
 
-/* This function runs once per frame, and is the heart of the program. */
-SDL_AppResult SDL_AppIterate(void* appstate)
-{
-    const double now = ((double)SDL_GetTicks()) / 1000.0;  /* convert from milliseconds to seconds. */
-    /* choose the color for the frame we will draw. The sine wave trick makes it fade between colors smoothly. */
-    const float red = (float)(0.5 + 0.5 * SDL_sin(now));
-    const float green = (float)(0.5 + 0.5 * SDL_sin(now + SDL_PI_D * 2 / 3));
-    const float blue = (float)(0.5 + 0.5 * SDL_sin(now + SDL_PI_D * 4 / 3));
-    SDL_SetRenderDrawColorFloat(renderer, red, green, blue, SDL_ALPHA_OPAQUE_FLOAT);  /* new color, full alpha. */
 
-    /* clear the window to the draw color. */
-    SDL_RenderClear(renderer);
+void close() {
 
-    /* put the newly-cleared rendering on the screen. */
-    SDL_RenderPresent(renderer);
+	SDL_DestroySurface(gImage);
+	gImage = NULL;
 
-    return SDL_APP_CONTINUE;  /* carry on with the program! */
+
+	SDL_DestroyWindow(gWindow);
+	gWindow = NULL;
+
+	SDL_Quit();
 }
 
-/* This function runs once at shutdown. */
-void SDL_AppQuit(void* appstate, SDL_AppResult result)
-{
-    /* SDL will clean up the window/renderer for us. */
+
+int main(int argc, char* argv[]) {
+
+	if (!init())
+	{
+		std::cout << "Failed to init";
+	}
+	else 
+	{
+		if (!loadMedia())
+		{
+			std::cout << "Failed to load media";
+		}
+		else
+		{
+			SDL_BlitSurface(gImage, NULL , gScreenSurface, NULL);
+			SDL_UpdateWindowSurface(gWindow);
+			SDL_Event e; bool quit = false; while (quit == false) { while (SDL_PollEvent(&e)) { if (e.type == SDL_EVENT_QUIT) quit = true; } }
+		}
+
+	}
+
+		close();
+	
+	
+
+	return 0;
 }
